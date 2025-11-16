fa4ff338eeb9e28ee2fe4945cfc53dfc125cc6d0
Nguyên Trần <trandinhnguyen46159@gmail.com>
2025-11-16 14:53:16 +0700

    Add profile manage for user, Modified login

--- GreenConnectPlatform.Api/Configurations/ServiceConfiguration.cs
+++ GreenConnectPlatform.Api/Configurations/ServiceConfiguration.cs
@@ -2,8 +2,10 @@ using GreenConnectPlatform.Business.Mappers;
 using GreenConnectPlatform.Business.Services.Auth;
 using GreenConnectPlatform.Business.Services.FileStorage;
 using GreenConnectPlatform.Business.Services.Jwt;
+using GreenConnectPlatform.Business.Services.Profile;
 using GreenConnectPlatform.Business.Services.ScrapPosts;
 using GreenConnectPlatform.Business.Services.ScrapPosts.ScrapPostDetails;
+using GreenConnectPlatform.Data.Repositories.CollectorVerificationInfos;
 using GreenConnectPlatform.Data.Repositories.Profiles;
 using GreenConnectPlatform.Data.Repositories.ScrapCategories;
 using GreenConnectPlatform.Data.Repositories.ScrapPosts;
@@ -23,6 +25,7 @@ public static class ServiceConfiguration
         services.AddSingleton<IFileStorageService, FirebaseStorageService>();
         services.AddScoped<IScrapPostDetailService, ScrapPostDetailService>();
         services.AddScoped<IJwtService, JwtService>();
+        services.AddScoped<IProfileService, ProfileService>();
     }
 
     public static void ConfigureRepositories(this IServiceCollection services)
@@ -32,5 +35,6 @@ public static class ServiceConfiguration
         services.AddScoped<IScrapPostDetailRepository, ScrapPostDetailRepository>();
         services.AddScoped<IScrapCategoryRepository, ScrapCategoryRepository>();
         services.AddScoped<IProfileRepository, ProfileRepository>();
+        services.AddScoped<IVerificationInfoRepository, VerificationInfoRepository>();
     }
 }

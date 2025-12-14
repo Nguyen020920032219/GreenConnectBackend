25fc3093c67f2e7ef444ceccbba1ce626487bcb3
Tuan Anh <tuananh19081507@gmail.com>
2025-12-14 22:51:13 +0700

    Add CreditTransactionHistory service and controller for user transaction history

---
+++ GreenConnectPlatform.Api/Controllers/CreditTransactionHistoryController.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Claims;
+using GreenConnectPlatform.Business.Models.CreditTransactionHistories;
+using GreenConnectPlatform.Business.Models.Exceptions;
+using GreenConnectPlatform.Business.Models.Paging;
+using GreenConnectPlatform.Business.Services.CreditTransactionHistories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenConnectPlatform.Api.Controllers;
+
+[Route("credit-transaction")]
+[Tags("Credit Transaction History")]
+[ApiController]
+public class CreditTransactionHistoryController(ICreditTransactionHistoryService creditTransactionHistoryService) : ControllerBase
+{
+    /// <summary>
+    ///   (Collector) Xem danh sách lịch sử giao dịch tín dụng của mình.
+    /// </summary>
+    /// <param name="pageIndex"></param>
+    /// <param name="pageSize"></param>
+    /// <param name="sortByCreatedAt">Sắp xếp theo ngày đã tạo ra giao dịch</param>
+    /// <param name="type">Lọc theo loại giao dịch(Purchase, Usage, Refund, Bonus)</param>
+    /// <returns></returns>
+    [HttpGet]
+    [Authorize(Roles = "IndividualCollector, BusinessCollector")]
+    [ProducesResponseType(typeof(PaginatedResult<CreditTransactionHistoryModel>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionModel), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ExceptionModel), StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> GetCreditTransactionHistories(
+        [FromQuery] int pageIndex = 1,
+        [FromQuery] int pageSize = 10,
+        [FromQuery] bool sortByCreatedAt = true,
+        [FromQuery] string? type = null)
+    {
+        var userId = GetCurrentUserId();
+        var result = await creditTransactionHistoryService.GetCreditTransactionHistoriesByUserIdAsync(pageIndex, pageSize, userId, sortByCreatedAt, type);
+        return Ok(result);
+    }
+    
+    private Guid GetCurrentUserId()
+    {
+        var idStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        return Guid.TryParse(idStr, out var id) ? id : Guid.Empty;
+    }
+}
---
+++ GreenConnectPlatform.Business/Services/PaymentTransactions/PaymentTransactionService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using GreenConnectPlatform.Business.Models.Paging;
+using GreenConnectPlatform.Business.Models.PaymentTransactions;
+using GreenConnectPlatform.Data.Enums;
+using GreenConnectPlatform.Data.Repositories.PaymentTransactions;
+
+namespace GreenConnectPlatform.Business.Services.PaymentTransactions;
+
+public class PaymentTransactionService : IPaymentTransactionService
+{
+    private readonly IPaymentTransactionRepository _paymentTransactionRepository;
+    private readonly IMapper _mapper;
+
+    public PaymentTransactionService(IPaymentTransactionRepository paymentTransactionRepository, IMapper mapper)
+    {
+        _paymentTransactionRepository = paymentTransactionRepository;
+        _mapper = mapper;
+    }
+    
+    public async Task<PaginatedResult<PaymentTransactionModel>> GetPaymentTransactionsByUserAsync(int pageIndex, int pageSize, Guid userId, bool sortByCreatedAt,
+        PaymentStatus? status = null)
+    {
+        var (items, totalCount) = await _paymentTransactionRepository
+            .GetPaymentTransactionsByUserId(pageIndex, pageSize, userId, sortByCreatedAt, status);
+        var models = _mapper.Map<List<PaymentTransactionModel>>(items);
+        return new PaginatedResult<PaymentTransactionModel>
+        {
+            Data = models,
+            Pagination = new  PaginationModel(totalCount, pageIndex, pageSize)
+        };
+    }
+
+    public async Task<PaginatedResult<PaymentTransactionModel>> GetPaymentTransactionsAsync(int pageIndex, int pageSize, bool sortByCreatedAt, PaymentStatus? status,
+        DateTime startDate, DateTime endDate)
+    {
+        var (items, totalCount) = await _paymentTransactionRepository
+            .GetAllPaymentTransactions(pageIndex, pageSize, sortByCreatedAt, status, startDate, endDate);
+        var models = _mapper.Map<List<PaymentTransactionModel>>(items);
+        return new PaginatedResult<PaymentTransactionModel>
+        {
+            Data = models,
+            Pagination = new  PaginationModel(totalCount, pageIndex, pageSize)
+        };
+    }
+}

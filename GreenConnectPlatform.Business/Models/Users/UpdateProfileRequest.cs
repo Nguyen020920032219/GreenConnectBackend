cb20048987c0750fa716fa27613cee2aff28c358
Tuan Anh <tuananh19081507@gmail.com>
2025-12-14 16:17:06 +0700

    Add location handling to profile updates with geometry support

--- GreenConnectPlatform.Business/Models/Users/UpdateProfileRequest.cs
+++ GreenConnectPlatform.Business/Models/Users/UpdateProfileRequest.cs
@@ -1,3 +1,4 @@
+using GreenConnectPlatform.Business.Models.ScrapPosts;
 using GreenConnectPlatform.Data.Enums;
 
 namespace GreenConnectPlatform.Business.Models.Users;
@@ -8,6 +9,7 @@ public class UpdateProfileRequest
     public string? Address { get; set; }
     public Gender? Gender { get; set; }
     public DateOnly? DateOfBirth { get; set; }
+    public LocationModel? Location { get; set; }
     public string? BankCode { get; set; }
     public string? BankAccountNumber { get; set; }
     public string? BankAccountName { get; set; }
